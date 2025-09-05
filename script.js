0d51c2e823049617c1764335005f48f96166c5d4
Paulobressane <paulo_marciano@estudante.sesisenai.org.br>
2025-09-04 22:02:52 -0300

    descrição do commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// alert("olá mundo !")
+// alert("oi")
+// alert("tudo bem")
+// alert("você vem sempre aqui ?")
+
+let idade
+idade = 18
+alert(idade)
+
+
+idade = "melancia"
+alert(idade)
+
+idade= 18.1
+alert(idade)
+CONST AMOR= "MEU PRIMEIRO AMOR"
+alert(AMOR)
+
+
+alert(AMOR)
+LET AMOR2 = "MEU SEGUNDO AMOR"
+ VAR ANTIGAO = "NUNCA USAR ESSA PAIOCA" 
+
+
+
+
+
