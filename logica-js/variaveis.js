8bf0c44502e34d987c1b0b8b809127e8d393e365
Paulobressane <paulo_marciano@estudante.sesisenai.org.br>
2025-09-09 22:19:34 -0300

    variaveis em javascript

---
+++ logica-js/variaveis.js
@@ -0,0 +1,24 @@
+// VARIÁVEL COM VAR  
+// var nome = "Paulo"
+// var nome = "Roberto" 
+// nome = "Roberto"
+
+// VARIAVEL COM LET 
+// let nome= "PAULO"
+// nome= "Roberto" 
+
+//variavel declarada com const 
+const nome = "Paulo" // string
+const sobrenome = "bressane"
+const idade = 50 // number 
+const masculino = true //boolean
+
+//array
+// const frutas = ["laranja" ," maçã" ,"banana"]
+
+console.log(frutas[0]): 
+
+
+// console.log("meu nome é "+ nome + " " + sobrenome + " e minha idade é " + idade + " anos")
+
+// console.log(`meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos`) 
