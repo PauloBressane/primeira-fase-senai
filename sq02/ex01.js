3740ee9b54a448e3d44f443fc3f3139fdb127823
Paulobressane <paulo_marciano@estudante.sesisenai.org.br>
2025-09-10 22:25:40 -0300

    exercicio aula Rafael

---
+++ sq02/ex01.js
@@ -0,0 +1,25 @@
+// let idade
+ 
+// idade = Number( prompt("digite sua idade :"))
+
+
+// if(idade >= 18){
+//     alert("Maior de idade")
+//     alert("pode sair pro intervalo")
+// }else{
+//     alert("dimenó")
+//     alert("pode sair pro recreio")
+
+    
+// }
+
+// let numero
+
+// numero = Number(prompt("Digite o número :"))
+// if(numero % 2 == 0){
+//     alert("par")
+// }else{
+//     alert("impar")
+// }
+
+
