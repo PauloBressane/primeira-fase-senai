e0650846270ffd98d839fdcd9af6dabce4fe16f5
Paulobressane <paulo_marciano@estudante.sesisenai.org.br>
2025-09-15 22:19:59 -0300

    exercicio de hoje

---
+++ sq05/ex51.js
@@ -0,0 +1,27 @@
+let senha
+
+senha = prompt("digite a senha:")
+    if(senha == 1234){
+        alert("acesso permitido")
+    }else{
+        alert("acesso negado")
+    }
+
+
+    let A , B
+     A = number(prompt("digite o primeiro número"))
+     B = number(prompt("digite o segundo número"))
+
+    if(A > B){
+        alert(" primeiro: "+ A )
+        // alert("primeiro ("+ A +" )é maior que o segundo("+ B +"))
+    }else if(B > A ){
+        alert("segundo (" + B + ") é maior que o primeiro(" + A +")")
+    }else{
+        alert("saõ iguais")
+
+    }
+        
+    
+     
+    }
