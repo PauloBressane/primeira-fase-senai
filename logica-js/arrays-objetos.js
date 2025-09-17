6516dae63875fd662bd753933a780e37bf477007
Paulobressane <paulo_marciano@estudante.sesisenai.org.br>
2025-09-16 22:14:11 -0300

    arrays e objetos

---
+++ logica-js/arrays-objetos.js
@@ -0,0 +1,26 @@
+const alunoDoSenai = [
+    {
+        nome: "João",
+        email:"joao@gmail.com",
+        matricula:"123",
+        idade:40
+    },
+    {
+        nome:"Maria",
+        email:"maria@gmail.com",
+        idade:30
+    },
+    {
+        nome:"Paulo",
+        email:"paulo@gmail.com",
+        idade:50
+    }
+
+  
+]
+
+// console.log(alunoDoSenai)
+
+console.log(` o nome do usuario é ${alunoDoSenai[1].nome}`)
+
+  console.log(`Meu nome é ${alunoDoSenai[2].nome}`)
---
+++ logica-js/objetos.js
@@ -0,0 +1,11 @@
+const usuario = {
+    login:"username",
+    email:"email@mail.com",
+    saldo:50000.00,
+    islogin:true,
+    senha:"123456789"
+}
+
+console.log(usuario.email)
+
+console.log(`o email do usuario é ${usuario.email} o saldo do usuario é ${usuario.saldo}`)
