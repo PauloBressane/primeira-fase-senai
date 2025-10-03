bed31c83aac9f74d5eb11b83598e134e4fe6f464
PAULO ROBERTO BRESSANE MARCIANO <paulo_marciano@estudante.sesisenai.org.br>
2025-10-02 21:30:14 -0300

    exercicios fort.js

---
+++ logica-js/repeticao/for.js
@@ -0,0 +1,37 @@
+// alert("for")
+// let i = 0
+// while(i<3){
+//     alert("valor do i"+ i)
+//     i++
+
+// }
+
+// for(let i = 0; i ++){
+//     alert("valor do i:" + i)
+
+// }
+
+// let soma = 0
+// let nota1 = Number(promt("digita a nota:"))
+// soma = nota1
+// let nota2 = Number(prompt("digite a ai a segunda 2a:"))
+// soma = soma + nota 2
+// let nota3 = Number(prompt("digite a nota:"))
+// soma = soma + nota3
+
+
+// let soma 0
+// for( let i=0 i<10; i++ ){
+//     let nota = Number(prompt("digite a " + "nota:"))
+// soma = soma + nota 
+// }
+
+// alert("soma:" + soma)
+
+// const nome = ["Dona Bete","Mano Juca","Lucio Fernando","Capitao Ganso"]
+
+// for(let i=0; i,<4; i++){
+//     alert(nomes [i])
+// }
+
+
