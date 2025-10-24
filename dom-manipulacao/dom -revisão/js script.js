5357bee822d43cd8a44743d5bba070a967ae7ae8
Paulobressane <paulo_marciano@estudante.sesisenai.org.br>
2025-10-23 22:14:54 -0300

    exercicio html

---
+++ dom-manipulacao/dom -revisão/js script.js
@@ -0,0 +1,10 @@
+
+function cadastrarUsuario(){
+    //console,log("até aqui deu");
+    usuario = document.getElementById("inputUsuario").value
+
+     document.getElementById("inputUsuario").value=' '
+
+     document.getElementById("resultado").innerHTML = "usuario cadastrado com suceesso"+ usuario
+
+}
